18a03fae4765d11ed132ec1e9a866a23911f401b
ljven00 <jeanvenaldolouissaint@gmail.com>
2023-04-22 23:10:04 -0600

    Finish JavaScript file and complete project

---
+++ BasketballBoard/app.js
@@ -0,0 +1,41 @@
+
+// scores
+let homeScore = 0;
+let guessScore = 0;
+
+// scores label
+const home = document.getElementById("home");
+const guess = document.getElementById("guess");
+
+
+// event listener
+const btns = Array.from(document.querySelectorAll(".points"));
+
+for (const btn of btns) {
+    btn.addEventListener("click", addPoints);
+}
+
+
+// updating score
+
+function addPoints(e){
+    let btnClassName = e.target.className;
+    let pointName = Number(e.target.textContent.charAt(2));
+    let point;
+    switch(pointName){
+        case 1: point = 1;
+            break;
+        case 2: point = 2;
+            break;
+        default: point = 3;
+    }
+    
+
+    if(btnClassName.indexOf("home") != -1 ){
+        homeScore += point;
+        home.textContent = homeScore;
+    }else{
+        guessScore += point;
+        guess.textContent = guessScore;
+    }
+}
