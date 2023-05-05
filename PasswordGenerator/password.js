87fd4e10e0dc13a72298d4b881236b7343fb6b7d
ljven00 <jeanvenaldolouissaint@gmail.com>
2023-05-04 23:11:52 -0600

    App functional but lacking color

--- PasswordGenerator/password.js
+++ PasswordGenerator/password.js
@@ -1,11 +1,47 @@
+
+/* Password characters */
 const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lower = upper.toLowerCase();
 const symbol = "*/-+!#$%&()=?{}[]<>,.";
 const digits = 123456789;
 const sample = upper + digits + lower + symbol;
-let size = 15;
 
 
+
+// password length
+let size;
+const sizeEl = document.querySelector("#password-length");
+
+// passwords elements
+const passwordOne = document.querySelector(".one");
+const passwordTwo = document.querySelector(".two");
+
+
+// copying the password on click
+passwordOne.addEventListener("click", (e)=> {
+	e.target.focus;
+	navigator.clipboard.writeText(e.target.textContent)
+	alert("First password copied");
+});
+
+passwordTwo.addEventListener("click", (e)=> {
+	e.target.focus;
+	navigator.clipboard.writeText(e.target.textContent)
+	alert("Second password copied");
+});
+
+// generate password button
+const generateBtn = document.querySelector("#gen-btn");
+
+generateBtn.addEventListener("click", ()=>{
+	size = Number(sizeEl.value);
+	if(size < 15)
+		size = 15;
+	passwordOne.textContent = getSample(sample, size);
+	passwordTwo.textContent = getSample(sample, size);
+});
+
+/* takes a string and a length to generate a random password */
 function getSample(w, s){
 	let string = "";
 	let r;
@@ -18,4 +54,4 @@ function getSample(w, s){
 
 let password = getSample(sample, size);
 
-console.log(password);
+
