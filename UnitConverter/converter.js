c426d94af38d38a56a3acffb92f0a9a42456c4ea
ljven00 <jeanvenaldolouissaint@gmail.com>
2023-05-07 12:08:29 -0600

    Update project to use Jquery

--- UnitConverter/converter.js
+++ UnitConverter/converter.js
@@ -1,23 +1,43 @@
 
 //input value
-const valueEl = document.querySelector("#value");
+const valueEl = $("#value");
+// const valueEl = document.querySelector("#value");
 let value;
+
 
 // placeholder of value to be converted
 const valToBeConvertedEl = Array.from(document.querySelectorAll(".value"));
 
 // value converted
-const feetEl = document.querySelector("#feet");
-const metersEl = document.querySelector("#meters");
-const gallonsEl = document.querySelector("#gallons");
-const litersEl = document.querySelector("#liters");
-const poundsEl = document.querySelector("#pounds");
-const kilosEl = document.querySelector("#kilos");
+const feetEl = $("#feet");
+// const feetEl = document.querySelector("#feet");
+const metersEl = $("#meters");
+// const metersEl = document.querySelector("#meters");
+const gallonsEl = $("#gallons");
+// const gallonsEl = document.querySelector("#gallons");
+const litersEl = $("#liters");
+// const litersEl = document.querySelector("#liters");
+const poundsEl = $("#pounds");
+// const poundsEl = document.querySelector("#pounds");
+const kilosEl = $("#kilos");
+// const kilosEl = document.querySelector("#kilos");
 
 // convert button
-const convertBtn = document.querySelector("#convert-btn");
+const convertBtn = $("#convert-btn");
+// const convertBtn = document.querySelector("#convert-btn");
 
 // convert click event
+convertBtn.click(function(){
+    value = Number(valueEl.val());
+    if(isNaN(value) || value <= 0){
+        value = 20;
+        valueEl.val(value);
+    }
+    valueEl.val("");
+    renderConversion(value)
+});
+
+/*
 convertBtn.addEventListener("click", () =>{
     value = Number(valueEl.value);
     if(isNaN(value) || value <= 0){
@@ -32,8 +52,22 @@ convertBtn.addEventListener("click", () =>{
     kilosEl.textContent = toKilos(value);
     renderInput(value, valToBeConvertedEl);
 });
+*/
 
-// conversion function
+// render conversion function
+
+function renderConversion(val){
+    feetEl.text(toFeet(val));
+    metersEl.text(toMeters(val));
+    gallonsEl.text(toGallons(val));
+    litersEl.text(toLiters(val));
+    poundsEl.text(toPounds(val));
+    kilosEl.text(toKilos(val));
+    renderInput(value, valToBeConvertedEl);
+}
+
+
+// conversion functions
 
 /* return the feet measures of the given meter */
 function toFeet(n){
